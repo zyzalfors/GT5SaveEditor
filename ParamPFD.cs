4e3b641aa8e34ed764310614e0e387568f2b1042
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2024-11-03 14:38:08 +0100

    Add files via upload

--- ParamPFD.cs
+++ ParamPFD.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Text;
 using System.Security.Cryptography;
 
-public class PFDHeader {
+    public class PFDHeader {
         public ulong magic;
         public ulong version;
     }
